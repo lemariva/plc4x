1dcf0861d9c03a5de2d56b43c7eb8ce6c6b67ee4
Łukasz Dywicki <luke@code-house.org>
2021-04-20 01:49:18 +0200

    PLC4X-294 / PLC4X-296 Work in progress.

--- plc4j/spi/src/main/java/org/apache/plc4x/java/spi/connection/DefaultNettyPlcConnection.java
+++ plc4j/spi/src/main/java/org/apache/plc4x/java/spi/connection/DefaultNettyPlcConnection.java
@@ -21,8 +21,12 @@ package org.apache.plc4x.java.spi.connection;
 import io.netty.channel.*;
 import io.netty.util.HashedWheelTimer;
 import io.netty.util.Timer;
+import org.apache.plc4x.java.api.EventPlcConnection;
 import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
 import org.apache.plc4x.java.api.exceptions.PlcIoException;
+import org.apache.plc4x.java.api.listener.ConnectionStateListener;
+import org.apache.plc4x.java.api.listener.EventListener;
+import org.apache.plc4x.java.api.value.PlcValueHandler;
 import org.apache.plc4x.java.spi.configuration.Configuration;
 import org.apache.plc4x.java.spi.configuration.ConfigurationFactory;
 import org.apache.plc4x.java.spi.events.*;
@@ -31,11 +35,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.plc4x.java.api.value.PlcValueHandler;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
-public class DefaultNettyPlcConnection extends AbstractPlcConnection implements ChannelExposingConnection {
+public class DefaultNettyPlcConnection extends AbstractPlcConnection implements ChannelExposingConnection, EventPlcConnection {
 
     /**
      * a {@link HashedWheelTimer} shall be only instantiated once.
@@ -50,6 +57,7 @@ public class DefaultNettyPlcConnection extends AbstractPlcConnection implements 
     protected final boolean awaitSessionSetupComplete;
     protected final boolean awaitSessionDisconnectComplete;
     protected final ProtocolStackConfigurer stackConfigurer;
+    protected final List<EventListener> listeners = new CopyOnWriteArrayList<>();
     protected final CompletableFuture<Void> sessionDisconnectCompleteFuture = new CompletableFuture<>();
 
     protected Channel channel;
@@ -170,10 +178,14 @@ public class DefaultNettyPlcConnection extends AbstractPlcConnection implements 
                 pipeline.addLast(new ChannelInboundHandlerAdapter() {
                     @Override
                     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+                        Stream<ConnectionStateListener> eventListeners = listeners.stream().filter(ConnectionStateListener.class::isInstance)
+                            .map(ConnectionStateListener.class::cast);
                         if (evt instanceof ConnectedEvent) {
                             sessionSetupCompleteFuture.complete(null);
+                            eventListeners.forEach(ConnectionStateListener::connected);
                         } else if (evt instanceof DisconnectedEvent) {
                             sessionDisconnectCompleteFuture.complete(null);
+                            eventListeners.forEach(ConnectionStateListener::disconnected);
                         } else {
                             super.userEventTriggered(ctx, evt);
                         }
@@ -191,6 +203,16 @@ public class DefaultNettyPlcConnection extends AbstractPlcConnection implements 
         logger.trace("Channel was created, firing ChannelCreated Event");
         // Send an event to the pipeline telling the Protocol filters what's going on.
         channel.pipeline().fireUserEventTriggered(new ConnectEvent());
+    }
+
+    @Override
+    public void addEventListener(EventListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void removeEventListener(EventListener listener) {
+        listeners.remove(listener);
     }
 
 }
